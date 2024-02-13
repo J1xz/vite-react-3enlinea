bacc896064ccdf39a96188232edb4704b9685ff6
Fernando <Fer_namtz@hotmail.com>
2024-02-13 12:36:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+
+  //ACTUALIZACION DE ESTADO CON CLICK
+  const handleClick = () => {
+    updateBoard(index);
+  };
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
+};
+function App() {
+  //ESTADO INICIAL
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  //PRIMER TURNO X
+  const [turn, setTurn] = useState(TURNS.X);
+
+  //NUEVO TURNO
+  const updateBoard = (index) => {
+    // GUARDADO DE POSICION EN INDICE
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  };
+  return (
+    <main className="board">
+      <h1>3 en linea</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            //UPDATE NO SE PASA COMO FUNCION SINO COMO PARAMETRO
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
+  );
+}
+
+export default App;
